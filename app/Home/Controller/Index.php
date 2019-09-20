56b251ad6db3819a39067687d39fcffffb3390ce
zhangbofa <626223818@qq.com>
2019-09-20 17:40:45 +0800

    model 初实现

--- app/Home/Controller/Index.php
+++ app/Home/Controller/Index.php
@@ -8,19 +8,15 @@
 
 namespace App\Home\Controller;
 
+use App\Home\Model\User;
 use Happgo\Db\DbPool;
 
 class Index
 {
     public function index()
     {
-        $connect = DbPool::getInstance()->get();
-        $stmt = $connect->prepare('SELECT * FROM h_user WHERE id = ?');
-        if ($stmt == false) {
-            var_dump($connect->errno, $connect->error);
-        }
-        $res = $stmt->execute([1]);//绑定参数 执行sql
-        DbPool::getInstance()->release($connect);//释放回收链接
+        $res = User::find(1);
+
         return ['msg' => $res];
     }
 }
---
+++ app/Home/Model/User.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA
+ * Author: 张伯发
+ * Date  : 2019/9/20
+ * Time  : 11:55
+ */
+
+namespace App\Home\Model;
+
+
+use Happgo\Db\Eloquent\Model;
+
+
+class User extends Model
+{
+
+
+
+
+}
